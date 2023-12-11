235e51c9134f0431c3c77f028812aadf91819cb5
Mateo Congo <aidenperse@gmail.com>
2023-12-10 19:51:01 -0500

    registro/Cliente - Propietario

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import './App.css'
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Auth from './layout/Auth';
-//Páginas
+// Páginas
+import { NotFound } from './pages/NotFound';
 import Login from './pages/Login';
 import { LandingPage } from './pages/LandiPage';
-import {Forgot} from './pages/Forgot';
+import { Forgot } from './pages/Forgot';
+import { RegisterCliente } from './components/Cliente/RegisterCliente'; 
+import { RegisterPropietario} from './components/Propietario/RegisterPropietario';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-    
-        
         <Routes>
-            
-
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/login" element={<Login />} />
-            <Route path='/forgot/:id' element={<Forgot />} />
+          <Route path='*' element={<NotFound/>}/>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/forgot/:id' element={<Forgot />} />
+          <Route path='/register/Cliente' element={<RegisterCliente />} />
+          <Route path='/register/Propietario' element={<RegisterPropietario />} />
         </Routes>
-
       </div>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Mensaje from '../components/Alertas/Mensaje';
-import autoLoginImage from '../assets/images/autoLogin.jpg'; 
+import autoLoginImage from '../assets/images/autoLogin.jpg';
 
 const Login = () => {
-const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [selectedRole, setSelectedRole] = useState(null);
   const [mensaje, setMensaje] = useState({});
   const [form, setForm] = useState({
     email: '',
     password: '',
   });
+
+  const navigate = useNavigate();
+  const [mensajeCliente, setMensajeCliente] = useState(null);
+  const [mensajePropietario, setMensajePropietario] = useState(null);
 
   const handleChange = (e) => {
     setForm({
@@ -21,16 +25,24 @@ const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Tu lógica de autenticación aquí
+    // Lógica de autenticación aquí
     // ...
 
     // Ejemplo de redirección
     navigate('/dashboard');
   };
 
+  const handleRegisterClick = (role) => {
+    setSelectedRole(role);
+  };
+
+  const closeModal = () => {
+    setSelectedRole(null);
+  };
+
   return (
     <>
-    <div className="flex font-serif h-screen">
+      <div className="flex font-serif h-screen">
         <div
           className={`hidden sm:block w-full md:w-1/2 h-screen bg-cover bg-center bg-no-repeat`}
           style={{ backgroundImage: `url(${autoLoginImage})` }}
@@ -41,6 +53,7 @@ const [showPassword, setShowPassword] = useState(false);
             {Object.keys(mensaje).length > 0 && (
               <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
             )}
+
             <h1 className="text-3xl font-semibold mb-2 text-center uppercase text-gray-500">
               Bienvenido
             </h1>
@@ -48,23 +61,21 @@ const [showPassword, setShowPassword] = useState(false);
               Bienvenido de nuevo! Por favor ingresa tus datos
             </small>
 
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
-        <div className="mb-3">
-          <label className="mb-2 block text-sm font-semibold">Correo electrónico</label>
-          <input
-            type="email"
-            placeholder="Introduce tu correo electrónico"
-            name='email'
-            value={form.email || ""}
-            onChange={handleChange}
-            className="w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
-          />
-        </div>
+            <form onSubmit={handleSubmit} className="w-full max-w-md">
+              <div className="mb-3">
+                <label className="mb-2 block text-sm font-semibold">Correo electrónico</label>
+                <input
+                  type="email"
+                  placeholder="Introduce tu correo electrónico"
+                  name='email'
+                  value={form.email || ""}
+                  onChange={handleChange}
+                  className="w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
+                />
+              </div>
 
-        <div className="mb-3">
-                <label className="mb-2 block text-sm font-semibold">
-                  Contraseña
-                </label>
+              <div className="mb-3">
+                <label className="mb-2 block text-sm font-semibold">Contraseña</label>
                 <input
                   type={showPassword ? 'text' : 'password'}
                   placeholder="********************"
@@ -82,39 +93,77 @@ const [showPassword, setShowPassword] = useState(false);
                 </button>
               </div>
 
-        <div className="my-4">
-          <button className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">
-            Iniciar sesión
-          </button>
-        </div>
-      </form>
+              <div className="my-4">
+                <button className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">
+                  Iniciar sesión
+                </button>
+              </div>
+            </form>
 
-      <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
-        <hr className="border-gray-400" />
-        <p className="text-center text-sm"></p>
-        <hr className="border-gray-400" />
-      </div>
+            <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
+              <hr className="border-gray-400" />
+              <p className="text-center text-sm"></p>
+              <hr className="border-gray-400" />
+            </div>
 
-      <div className="mt-3 text-xs border-b-2 py-2">
-  <Link
-    to="/forgot/id"
-    className="underline text-sm text-gray-400 hover:text-gray-900"
-  >
-    ¿Olvidaste tu contraseña?
-  </Link>
-</div>
-
-
-      <div className="mt-3 text-sm flex justify-between items-center">
-              <p>¿No tienes una cuenta?</p>
+            <div className="mt-3 text-xs border-b-2 py-2">
               <Link
-                to="/register"
-                className="py-2 px-5 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white"
+                to="/forgot/id"
+                className="underline text-sm text-gray-400 hover:text-gray-900"
               >
-                Crear una cuenta nueva
+                ¿Olvidaste tu contraseña?
               </Link>
             </div>
 
+            <div className="mt-3 text-sm">
+              <p className="mb-2">¿No tienes una cuenta?</p>
+              <button
+                className="block w-full py-2 px-5 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white mb-2"
+                onClick={() => {
+                  handleRegisterClick('Cliente');
+                  setMensajeCliente("Seleccionaste crear una cuenta como Cliente");
+                  setMensajePropietario(null);
+                }}
+              >
+                Crear una cuenta (Cliente)
+              </button>
+              
+              
+              <button
+                className="block w-full py-2 px-5 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white"
+                onClick={() => {
+                  handleRegisterClick('Propietario');
+                  setMensajePropietario("Seleccionaste crear una cuenta como Propietario");
+                  setMensajeCliente(null);
+                }}
+              >
+                Crear una cuenta (Propietario)
+              </button>
+              
+            </div>
+      {/* Modal de registro */}
+      {selectedRole && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-8 rounded-md">
+            <p className="text-xl font-semibold mb-4">Selecciona tipo de cuenta</p>
+            <div className="flex justify-between">
+              {/* Puedes ajustar los enlaces según tus rutas */}
+              <Link
+                to={`/register/${selectedRole}`}
+                className="py-2 px-5 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white mr-4"
+              >
+                Continuar como {selectedRole}
+              </Link>
+              <button
+                className="py-2 px-5 bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white"
+                onClick={closeModal}
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
             <div className="mt-3 text-sm">
               <Link
                 to="/"
